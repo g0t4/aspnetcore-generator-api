a0b4520f9aee4a7dab69d7034cd3966eab2a8b60
Wes Higbee <wes.mcclure@gmail.com>
2017-06-07 18:43:31 -0700

    Add an action to send random names to an email address

--- api/Controllers/GeneratorController.cs
+++ api/Controllers/GeneratorController.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Faker;
 using Faker.Extensions;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 
 namespace api.Controllers
 {
@@ -12,6 +15,29 @@ namespace api.Controllers
     [Route("[action]")]
     public class GenerateController : Controller
     {
+        public const string MAIL_HOST = "localhost";
+        public const int MAIL_PORT = 1025;
+
+        [HttpPost]
+        public async Task EmailRandomNames(Range range, string email = "test@fake.com")
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Generator", "generator@generate.com"));
+            message.To.Add(new MailboxAddress("", email));
+            message.Subject = "Here are your random names!";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = string.Join(Environment.NewLine, range.Of(Name.FullName))
+            };
+            using (var mailClient = new SmtpClient())
+            {
+                await mailClient.ConnectAsync(MAIL_HOST, MAIL_PORT, SecureSocketOptions.None);
+                await mailClient.SendAsync(message);
+                await mailClient.DisconnectAsync(true);
+            }
+        }
+        
         [HttpGet]
         public IEnumerable<string> Names(Range range)
             => range.Of(Name.FullName);
