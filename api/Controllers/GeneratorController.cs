8650bb7dede2a2c5df1689ce16ac2867ea53203a
Konstantin Filimonenkov <kfilimonenkov@gmail.com>
2019-03-31 00:38:31 +0100

    adding sending emails and updating docker-compose

--- api/Controllers/GeneratorController.cs
+++ api/Controllers/GeneratorController.cs
@@ -1,6 +1,11 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Faker;
+using System.Threading.Tasks;
+using MimeKit;
+using System;
+using MailKit.Security;
+using MailKit.Net.Smtp;
 
 namespace api.Controllers
 {
@@ -8,6 +13,36 @@ namespace api.Controllers
     [Route("[action]")]
     public class GenerateController : ControllerBase
     {
+        public string MAIL_HOST = "mail";
+        public int MAIL_PORT = 1025;
+
+        //public GenerateController(IOptions<MailServerConfig> mailServerConfigAccessor)
+        //{
+        //    var config = mailServerConfigAccessor.Value;
+        //    MAIL_HOST = config.Host;
+        //    MAIL_PORT = config.Port;
+        //}
+
+        [HttpPost]
+        public async Task EmailRandomNames(Range range, string email = "test@fake.com")
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Generator", "generator@generate.com"));
+            message.To.Add(new MailboxAddress("", email));
+            message.Subject = "Here are your random names!";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = string.Join(Environment.NewLine, range.Of(Name.FullName))
+            };
+            using (var mailClient = new SmtpClient())
+            {
+                await mailClient.ConnectAsync(MAIL_HOST, MAIL_PORT, SecureSocketOptions.None);
+                await mailClient.SendAsync(message);
+                await mailClient.DisconnectAsync(true);
+            }
+        }
+
         [HttpGet]
         public IEnumerable<string> Names(Range range)
             => range.Of(Name.FullName);
