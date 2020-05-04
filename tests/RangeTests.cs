db4b8843ba73c655694ed88ea054bf54caf8be22
Derek Sadler <derek.sadler@salesforce.com>
2020-05-04 14:23:08 -0400

    WIP - TC Build Testing...
    1. let's see what we get;

---
+++ tests/RangeTests.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using Xunit;
+using Range = api.Controllers.Range;
+
+namespace tests
+{
+    public class RangeTests
+    {
+        [Fact]
+        public void CountShouldControlNumberOfResults()
+        {
+            var range = new Range {  Count = 3 };
+
+            var generated = range.Of(() => "");
+
+            Assert.Equal(3, generated.Count());
+        }
+
+        [Fact]
+        public void SortShouldOrderResults()
+        {
+            var range = new Range { Count = 3, Sort = true };
+            var values = new[] { "a", "c", "b" };
+            var counter = 0;
+            var generated = range.Of(() => values[counter++]);
+
+            Assert.Equal(new[] { "a", "b", "c" }, generated.ToArray());
+        }
+    }
+}
